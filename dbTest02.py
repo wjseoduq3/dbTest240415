fa4797ef77ddc5392d24f0bb1ced89380d6e6951
wjseoduq2 <dragon65@naver.com>
2024-04-19 10:18:27 +0900

    오류발생

--- dbTest02.py
+++ dbTest02.py
@@ -1,29 +1,32 @@
 import pymysql  # mysql과 연동시켜주는 라이브러리
 
 # 파이썬과 mysql간에 커넥션 생성(4가지 필요)
-# 1) 계정 guest01
+# 1) 계정 guest01 (본인PC는 root계정)
 # 2) 비밀번호 12345
 # 3) 데이터베이스가 설치된 컴퓨터 IP 주소:
-# 본인 컴퓨터: localhost
+# 본인 컴퓨터: localhost(127.0.0.1 쳐도 됨)
 # 교수용컴퓨터: 192.168.0.100
-# 4) 데이터베이스 스키마의 이름: 예: shopdb
+# port: 3306
+# 4) 데이터베이스 스키마의 이름: 예: shopdbjdy
 
 #  커넥션 생성
 dbConn = pymysql.connect(host='192.168.0.100', user='guest01', password='12345', db='shopdbjdy')
 
-sql = "INSERT INTO membertbl VALUES('lion', '김호랑', '인천 동구')"  #DB에 실행할 sql문 생성
+sql = "INSERT INTO membertbl VALUES('pion', '김호랑', '인천 동구')"  #DB에 실행할 sql문 생성/
+# 매번 아이디 변경해줘야 중복 에러 안남
+
 cur = dbConn.cursor()
-cur.execute(sql)  # 연결된 DB의 스키마에 지정된 sql문 실행
+result = cur.execute(sql)  # 연결된 DB의 스키마에 지정된 sql문 실행
 # result = cur.execute(sql)이 성공적으로 수행되면
 # print(result) 하면 1이 출력됨 (insert, delete, update)
-# if result == 1:
-#     print('회원가입이 성공하였습니다.')
+if result == 1:
+    print('회원가입이 성공하였습니다.')
 
-# records = cur.fetchall()  # sql에서 실행된 select문의 결과를 받아서 records로 저장
+records = cur.fetchall()  # sql에서 실행된 select문의 결과를 받아서 records로 저장
 # print(records)  # 결과는 튜플로 옴
 # print(records[0])
 # print(records[0][1])
-
+#
 # for member in records:
 #     print(member[1])
 
---
+++ memberDB.py
@@ -0,0 +1,42 @@
+import pymysql
+
+dbConn = pymysql.connect(user='guest01', password='12345', host='192.168.0.100', db='shopdbjdy')
+
+while True:
+    print("************ 회원관리 프로그램 ****************")
+    print("1: 회원 가입")
+    print("2: 회원 비밀번호  수정")
+    print("3: 회원 탈퇴")
+    print("4: 전체 회원목록 조회")
+    print("5: 프로그램 종료")
+    print("****************************")
+    menuNum = input("메뉴 중 한 가지를 선택하세요(1~5): ")
+
+    if menuNum == "1":
+        print("회원정보를 입력하세요")
+        memberID = print("1) 회원ID를 입력하세요: ")
+        memberName = print("2) 회원이름을 입력하세요: ")
+        address = print("3) 회원주소를 입력하세요: ")
+
+        sql = f"INSERT INTO membertbl VALUES('{memberID}', '{memberName}', {address})"
+        cur = dbConn.cursor()
+        result = cur.execute(sql)
+
+        if result == 1:
+            print("축하합니다! 회원가입 성공하였습니다.")
+        else:
+            print("회원가입 실패입니다.")
+
+        cur.close()
+        dbConn.commit()
+
+    elif menuNum == 5:
+        print("프로그램을 종료합니다. 안녕히 가세요")
+        dbConn.close()
+        break
+
+    else:
+        print("잘못 입력하셨습니다.")
+
+
+
